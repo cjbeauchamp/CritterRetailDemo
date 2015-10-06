a6a081dca2ad922edd35fd6cf3029efea647b4d3
Chris Beauchamp <cjbeauchamp@gmail.com>
2015-10-06 11:56:14 -0700

    update

--- critterretail-web/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/chrisbeauchamp/Code/Crittercism/CritterRetail/critterretail-web/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
